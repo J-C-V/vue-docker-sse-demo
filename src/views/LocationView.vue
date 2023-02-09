0407dfc4c8aaa26c0622a6dea65297ea9bfa71f1
Jonny Vang <jonny-vang@web.de>
2023-02-09 10:18:38 +0100

    Check if Geolocation API is available

--- src/views/LocationView.vue
+++ src/views/LocationView.vue
@@ -1,3 +1,11 @@
+<script setup lang='ts'>
+    if ('geolocation' in navigator) {
+        console.log('Geolocation works.');
+    } else {
+        console.log('Gelocation doesn\'t work.');
+    }
+</script>
+
 <template>
     <h2>This is the location page</h2>
 </template>
